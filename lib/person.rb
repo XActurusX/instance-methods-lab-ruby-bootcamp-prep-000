a2a98f0eac0825a0f45a72ab82114d17788f32fe
John Jordan Shelley <jshelley553@gmail.com>
2019-06-11 02:14:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,14 @@ class Person
 
 
      def talk
-        puts "Hello World"
+        puts "Hello World!"
      end
 
     def walk
-      puts "The Person is walking!"
+      puts "The Person is walking"
     end
 end
 
 ada = Person.new
 ada.talk
-ada.walk 
+ada.walk
